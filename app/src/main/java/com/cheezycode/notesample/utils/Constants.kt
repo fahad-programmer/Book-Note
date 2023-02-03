abf739bd1a4ba34bb1db62240e7c6355abe44993
Fahad Malik <unexpectedprogrammer@gmail.com>
2023-02-04 04:01:13 +0500

    changes made to the code and ui of the app

--- app/src/main/java/com/cheezycode/notesample/utils/Constants.kt
+++ app/src/main/java/com/cheezycode/notesample/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.cheezycode.notesample.utils
 
 object Constants {
     const val TAG = "NOTESAPPTAG"
-    const val BASE_URL = "https://web-production-d2f9.up.railway.app/"
+    const val BASE_URL = "https://unexpectedprogrammer.pythonanywhere.com/"
     const val USER_TOKEN = "user_token"
     const val PREFS_TOKEN_FILE = "prefs_token_file"
     val BG_COLORS = arrayOf("#fef9ed", "#f2f8ff", "#edffee", "#fce9ff")
